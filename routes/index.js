cde7581e00e1d69c8fd730fafc9f333279892079
Soojin Kim <soojinkk98@gmail.com>
2023-04-19 14:47:56 +0900

    updated to github

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages:messages });
+});
+
+router.get('/new', function(req,res){
+  res.render('form', { title: 'New Message Form' })
+})
+
+router.post('/new', function(req, res) {
+  let messageUser = req.body.name
+  let messageText = req.body.message
+  messages.push({text: messageText, user:messageUser, added: new Date()});
+  res.redirect('/');
+})
+
+module.exports = router;
